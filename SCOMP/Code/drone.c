cd970176152a46228598546041d1d52f1ce3157c
josesousa123 <153511700+josesousa123@users.noreply.github.com>
2025-04-16 16:46:43 +0100

    Initial SCOMP Functions for all US

---
+++ SCOMP/Code/drone.c
@@ -0,0 +1,70 @@
+/* drone.c - Processo de simulação de um drone individual */
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+// US262 - Estrutura para comunicar posição
+typedef struct {
+	int x, y, z;
+	int drone_id;
+	int step;
+} Position;
+
+int drone_id;
+int stop = 0;
+
+// US263 - Handler para SIGUSR1 (colisão)
+void handle_collision(int sig) {
+	sigset_t mask;
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGTERM);
+	sigprocmask(SIG_BLOCK, &mask, NULL);
+
+	printf("[DRONE %d] Received SIGUSR1: Collision detected!\n", drone_id);
+	sleep(1);  // Simula processamento do sinal
+
+	sigprocmask(SIG_UNBLOCK, &mask, NULL);
+}
+
+// US263 - Handler para SIGTERM (terminar)
+void handle_terminate(int sig) {
+	printf("[DRONE %d] Received SIGTERM: Terminating...\n", drone_id);
+	stop = 1;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <drone_id>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	drone_id = atoi(argv[1]);
+
+	// US263 - Regista handlers de sinais
+	signal(SIGUSR1, handle_collision);
+	signal(SIGTERM, handle_terminate);
+
+	srand(getpid());
+
+	for (int step = 0; step < 20 && !stop; step++) {
+		Position pos;
+		pos.drone_id = drone_id;
+		pos.step = step;
+
+		// US262 - Geração aleatória da posição
+		pos.x = rand() % 10;
+		pos.y = rand() % 10;
+		pos.z = rand() % 5;
+
+		// US262/264 - Envia posição para processo pai
+		write(STDOUT_FILENO, &pos, sizeof(Position));
+		sleep(1);
+	}
+
+	printf("[DRONE %d] Finished execution.\n", drone_id);
+	return 0;
+}
---
+++ SCOMP/Code/report.c
@@ -0,0 +1,150 @@
+/* main.c - Simulador de movimento de drones */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "report.h"	// US265 - Geração do relatório
+
+#define MAX_DRONES 10
+#define MAX_STEPS 20
+
+typedef struct {
+	int x, y, z;
+	int drone_id;
+	int step;
+} Position;
+
+// US262 - Captura e armazenamento de posições no tempo
+Position positionMatrix[MAX_STEPS][MAX_DRONES];
+int num_drones;
+int pipefds[MAX_DRONES][2];
+pid_t drone_pids[MAX_DRONES];
+int collision_count = 0;
+const int COLLISION_THRESHOLD = 3;
+
+void handle_drone_signal(int sig) {
+	// Pode ser usado no futuro
+}
+
+// US263 - Log de colisões
+void log_collision(int step, int x, int y, int z) {
+	printf("[COLLISION] Step %d at position (%d,%d,%d)\n", step, x, y, z);
+	collision_count++;
+}
+
+// US263 - Deteção de colisões
+int check_collision(int step, Position new_pos) {
+	for (int i = 0; i < num_drones; i++) {
+		Position pos = positionMatrix[step][i];
+		if (pos.drone_id != -1 &&
+			 pos.x == new_pos.x &&
+			 pos.y == new_pos.y &&
+			 pos.z == new_pos.z) {
+			log_collision(step, new_pos.x, new_pos.y, new_pos.z);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+// US262 - Guardar posição na matriz de passos
+void save_position(int step, Position pos) {
+	for (int i = 0; i < num_drones; i++) {
+		if (positionMatrix[step][i].drone_id == -1) {
+			positionMatrix[step][i] = pos;
+			return;
+		}
+	}
+}
+
+int main(int argc, char* argv[]) {
+	// US261 - Verificação de argumentos
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <num_drones>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	num_drones = atoi(argv[1]);
+	if (num_drones > MAX_DRONES)
+		num_drones = MAX_DRONES;
+
+	// US262 - Inicialização da matriz de posições
+	for (int i = 0; i < MAX_STEPS; i++)
+		for (int j = 0; j < MAX_DRONES; j++)
+			positionMatrix[i][j].drone_id = -1;
+
+	// US261 - Criação de pipes e drones
+	for (int i = 0; i < num_drones; i++) {
+		if (pipe(pipefds[i]) == -1) {
+			perror("pipe");
+			exit(EXIT_FAILURE);
+		}
+
+		pid_t pid = fork();
+		if (pid == 0) {
+			// Processo filho (drone)
+			close(pipefds[i][0]);
+			dup2(pipefds[i][1], STDOUT_FILENO);
+			char id[10];
+			sprintf(id, "%d", i);
+			execl("./drone", "drone", id, NULL);
+			perror("execl failed");
+			exit(EXIT_FAILURE);
+		} else if (pid > 0) {
+			// Processo pai
+			close(pipefds[i][1]);
+			drone_pids[i] = pid;
+		} else {
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	// US264 - Simulação por passos de tempo (timesteps)
+	for (int step = 0; step < MAX_STEPS; step++) {
+		for (int i = 0; i < num_drones; i++) {
+			Position pos;
+			int bytes = read(pipefds[i][0], &pos, sizeof(Position));
+			if (bytes == sizeof(Position)) {
+				pos.step = step;
+
+				// US263 - Verifica colisão e envia sinal
+				if (check_collision(step, pos)) {
+					kill(drone_pids[i], SIGUSR1);
+				}
+
+				// US262 - Guarda posição
+				save_position(step, pos);
+			}
+		}
+
+		// US264 - Avança para próximo timestep
+		sleep(1);
+
+		// US263 - Verifica número de colisões
+		if (collision_count >= COLLISION_THRESHOLD) {
+			printf("Too many collisions. Terminating simulation...\n");
+			for (int i = 0; i < num_drones; i++) {
+				kill(drone_pids[i], SIGTERM);
+			}
+			break;
+		}
+	}
+
+	// US261 - Espera pelo fim dos drones
+	for (int i = 0; i < num_drones; i++) {
+		wait(NULL);
+	}
+
+	// US265 - Geração do relatório
+	generate_report("simulation_report.txt", positionMatrix, MAX_STEPS, num_drones, collision_count);
+
+	printf("Simulation ended. Total collisions: %d\n", collision_count);
+	return 0;
+}
