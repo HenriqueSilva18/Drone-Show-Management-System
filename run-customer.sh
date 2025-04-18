688437fbf58baaeabac804643ae5123af4b938ef
josemoreira1 <1220848@isep.ipp.pt>
2025-04-18 21:03:15 +0100

    Updated customer scripts #10 #11

--- run-customer.sh
+++ run-customer.sh
@@ -2,7 +2,7 @@
 
 #REM set the class path,
 #REM assumes the build was executed with maven copy-dependencies
-export BASE_CP=shodrone.app.customer.console/target/shodrone.app.user.console-1.4.0-SNAPSHOT.jar:shodrone.app.utente.console/target/dependency/*;
+export BASE_CP=shodrone.app.customer.console/target/shodrone.app.customer.console-1.4.0-SNAPSHOT.jar:shodrone.app.customer.console/target/dependency/*;
 
 #REM call the java VM, e.g,
 java -cp $BASE_CP lapr4.app.customer.console.CustomerApp
