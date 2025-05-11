0c8a3c943fb45860bd4380e02dae07b6d4866986
duartemachado5 <1240586@isep.ipp.pt>
2025-05-11 18:13:46 +0100

    US245 Almost Done #109

--- shodrone.app.backoffice.console/src/main/java/lapr4/app/backoffice/console/presentation/menu/ShowDesignerMenu.java
+++ shodrone.app.backoffice.console/src/main/java/lapr4/app/backoffice/console/presentation/menu/ShowDesignerMenu.java
@@ -5,6 +5,7 @@ import eapli.framework.actions.menu.Menu;
 import lapr4.app.backoffice.console.presentation.MainMenu;
 import lapr4.app.backoffice.console.presentation.authz.AddUserUI;
 import lapr4.app.backoffice.console.presentation.figure.AddFigureToCatalogueUI;
+import lapr4.app.backoffice.console.presentation.figure.AddFigureCategoryToCatalogueUI;
 
 public class ShowDesignerMenu extends MainMenu {
     private static final String RETURN_LABEL = "Return ";
@@ -23,6 +24,11 @@ public class ShowDesignerMenu extends MainMenu {
         final var menu = new Menu("Show Design >");
 
         menu.addItem(1, "Add Figure to Catalogue", new AddFigureToCatalogueUI()::show);
+        menu.addItem(2, "Add Figure Category to Catalogue", new AddFigureCategoryToCatalogueUI()::show);
+        //menu.addItem(3, "Edit Figure Category", new EditFigureCategoryUI()::show);
+        //menu.addItem(4, "List Figure Categories", new ListFigureCategoryUI()::show);
+        //menu.addItem(5, "Activate/Inactivate Figure Category", new ActivateInactivateFigureCategoryUI()::show);
+
         menu.addItem(EXIT_OPTION, RETURN_LABEL, Actions.SUCCESS);
 
         return menu;
