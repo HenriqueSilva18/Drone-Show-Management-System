65eff73cfc05dd32bb25ec33969b38ed42d7eef7
Henrique Miguel Gomes da Silva <127787369+HenriqueSilva18@users.noreply.github.com>
2025-05-27 15:31:19 +0100

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 796dd4261755460ec3c5a98e9aa494e3c8d75c41, reversing
    changes made to 952ee754b592f3c26508ccc2570aa232642abdd8.

--- shodrone.core/src/main/java/lapr4/showPrososalManagement/application/ShowProposalService.java
+++
@@ -1,4 +0,0 @@
-package lapr4.showPrososalManagement.application;
-
-public class ShowProposalService {
-}
--- shodrone.core/src/main/java/lapr4/showPrososalManagement/domain/ApprovalStatus.java
+++
@@ -1,5 +0,0 @@
-package lapr4.showPrososalManagement.domain;
-
-public enum ApprovalStatus {
-    ACCEPTED, REJECTED, PENDENT;
-}
